68c873e0d6c57ee11212cf8a986129f307916bea
Hrushi Patel <72564568+Hp-175@users.noreply.github.com>
2022-12-02 19:18:26 +0530

    gh

---
+++ Space_Geeks_Server/configure.js
@@ -0,0 +1,5 @@
+
+module.exports={
+    'secretKey':'12345-67890-09876-54321',
+    'mongoUrl':'mongodb+srv://Hrushi:Hc2505@cluster0.ysevp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
